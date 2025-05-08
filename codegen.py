d1a7fec515ed2726a0a68bb2ca17799314bc2839
Shreya Alajangi <shreya8ya@gmail.com>
2025-05-08 13:28:23 +0530

    Update codegen.py

--- codegen.py
+++ codegen.py
@@ -2,7 +2,7 @@ import streamlit as st
 from langchain.agents import initialize_agent, Tool
 from langchain.chat_models import ChatOpenAI
 from langchain.agents.agent_types import AgentType
-from langchain.tools.python.tool import PythonREPLTool
+from langchain_community.tools.python.tool import PythonREPLTool 
 import os
 
 # For environment variables
